64d0d241787c09c1e17a6029b286587ae4e22d22
Eduard <mansurov@technokratos.com>
2021-03-22 22:20:39 +0300

    connected my own DI

--- src/main/java/tech/itpark/proggerhub/Main.java
+++ src/main/java/tech/itpark/proggerhub/Main.java
@@ -1,17 +1,22 @@
 package tech.itpark.proggerhub;
 
+import tech.itpark.di.Container;
+import tech.itpark.proggerhub.repository.UserRepository;
+import tech.itpark.proggerhub.service.UserService;
+
+import java.lang.reflect.InvocationTargetException;
+
 public class Main {
     // psvm + Tab -> main
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalAccessException, InstantiationException, InvocationTargetException {
         // TODO: MVP -> Minimal Valuable Product
         // videos
         // users/owners <-> consumers
         // Domain Model -> предметная область
 
-        // 0. Plain Java (manual)
-        // 1. Dependency Lookup -> Service Registry
-        // 2. Dependency Injection -> Container:
-        // 2.1 Сам делал new
-        // 2.2 Найти нужные зависимости, если они требуются для new
+        Container container = new Container();
+        container.register(UserRepository.class, UserService.class);
+        container.wire();
+        System.out.println("finish");
     }
 }
