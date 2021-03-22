8e7d1ced2c167f953f3001b7aae3d99c31706284
Eduard <mansurov@technokratos.com>
2021-03-22 22:12:14 +0300

    initial

---
+++ src/main/java/tech/itpark/proggerhub/Main.java
@@ -0,0 +1,17 @@
+package tech.itpark.proggerhub;
+
+public class Main {
+    // psvm + Tab -> main
+    public static void main(String[] args) {
+        // TODO: MVP -> Minimal Valuable Product
+        // videos
+        // users/owners <-> consumers
+        // Domain Model -> предметная область
+
+        // 0. Plain Java (manual)
+        // 1. Dependency Lookup -> Service Registry
+        // 2. Dependency Injection -> Container:
+        // 2.1 Сам делал new
+        // 2.2 Найти нужные зависимости, если они требуются для new
+    }
+}
